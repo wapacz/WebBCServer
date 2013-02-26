de04ed251367b3ab8ead1a0e2c04841b7afafa2d
Michał Łapacz <michal.lapacz@gmail.com>
2013-02-26 21:56:29 +0100

    Initial version

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: catchers
+ *
+ * Created on February 26, 2013, 9:54 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
